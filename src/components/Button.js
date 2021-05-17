586a2001da424f7436824f2665ea5d276bd60e39
Harkirath Dhillon <har7173@gmail.com>
2021-05-17 13:20:28 +0100

    creating renderButton helper function to make our ColorContext wrap less messy

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
   renderSubmit(value) {
     return value === 'english' ? 'Submit' : 'Voorleggen'
   }
 
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+            {/* rather than static contextType we can use consumer to get information out of the context. Which we pass a function */}
+            <LanguageContext.Consumer>
+              {(value) => this.renderSubmit(value)}
+            </LanguageContext.Consumer>
+          </button>
+    )
+  }
+
   render() {
     return (
-      <button className="ui button primary">
-        {/* rather than static contextType we can use consumer to get information out of the context. Which we pass a function */}
-        <LanguageContext.Consumer>
-          {(value) => this.renderSubmit(value)}
-        </LanguageContext.Consumer>
-      </button>
+      // remember that we can only pass consumers a function, so we cut and pasted the other consumer into the function
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)
+        }
+        
+      </ColorContext.Consumer>
     )
   }
 }
