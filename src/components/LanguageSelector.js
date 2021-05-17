b73d5d27de456e361457af6a8ca78f37f753a81e
Harkirath Dhillon <har7173@gmail.com>
2021-05-17 16:02:44 +0100

    changing this.props to this.context as the information now comes from context

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class LangaugeSelector extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
+    console.log(this.context)
     return (
       <div>
         Select a Langauge:
-        <i className="flag us" onClick={() => this.props.onLanguageChange('english')} />
-        <i className="flag nl" onClick={() => this.props.onLanguageChange('dutch')} />
+        <i className="flag us" onClick={() => this.context.onLanguageChange('english')} />
+        <i className="flag nl" onClick={() => this.context.onLanguageChange('dutch')} />
       </div>
     )
   }
